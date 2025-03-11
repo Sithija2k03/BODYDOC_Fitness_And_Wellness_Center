c8cd7f0c481a421a769178c8eaef40760ca8afc7
H A Sithija Dilshan Wickramasinghe <162593144+Sithija2k03@users.noreply.github.com>
2025-03-11 12:31:02 +0530

    Add income management feature
    Add income controller, routes, and model for managing income data

---
+++ backend/models/incomeModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const incomeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50  
+    },
+    amount: {
+        type: Number,
+        required: true,
+        trim: true  
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 200  
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Income', incomeSchema);
