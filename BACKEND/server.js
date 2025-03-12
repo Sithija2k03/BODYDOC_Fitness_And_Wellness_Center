d0a735a38b6489b01fabf44d7afa5d027032bb4d
RanganaNR <ranganarajapaksha46@gmail.com>
2025-03-12 10:18:51 +0530

    server
    server.js

---
+++ BACKEND/server.js
@@ -0,0 +1,65 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const app = express();
+// require("dotenv").config();
+
+// const PORT = process.env.PORT || 8070;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// const URL = process.env.MONGODB_URL;
+
+// mongoose.connect(URL,{
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopologyL : true,
+//     useFindAndModify : false
+// });
+
+// const connection = mongoose.connection;
+// connection.once("open",()=>{
+//     console.log("Mongodb Connection Success!");
+// })
+
+// app.listen(PORT, ()=>{
+//     console.log('Server is up and running on port ${PORT}')
+// })
+
+process.on("SIGINT", () => {
+    console.log("Shutting down server...");
+    process.exit();
+});
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+// const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL)
+    .then(() => {
+        console.log("MongoDB Connection Success!");
+    })
+    .catch((err) => {
+        console.error("MongoDB Connection Error:", err);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
