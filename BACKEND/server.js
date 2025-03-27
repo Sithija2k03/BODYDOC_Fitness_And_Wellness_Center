421a096ce3787be5ff93e8e81718e1aeb04ab0af
H A Sithija Dilshan Wickramasinghe <162593144+Sithija2k03@users.noreply.github.com>
2025-03-27 18:37:45 +0530

    latest

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,60 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const cors = require('cors');
+const { connect } = require('http2');
+const connectDB = require('./db/db');
+const { readdirSync } = require('fs');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+// Middleware
 const app = express();
-require("dotenv").config();
-
-const PORT = process.env.PORT || 8070; //starting 8070 port or available port
-
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//connect database
-const URL = process.env.MONGODB_URL;
+// Routes
+readdirSync('./BACKEND/routes').map((r) => app.use('/api/v1', require(`./BACKEND/routes/${r}`)));
 
-mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log(" MongoDB Connection Success!");
-}).catch((error) => {
-    console.error(" MongoDB Connection Error:", error);
-});
+const server = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+};
 
-
-//connection
-const connection = mongoose.connection;
-connection.once("open", () =>{
-    console.log("Mongodb Connection success!");
-})
-
-
-
-
-//access phamacyInventory table file
-const phamacyInventoryRouter = require("./routes/phamacyInventory.js");
-//(http://localhost:8070/phamacyInventory)we use this for routes.phamacyInventory file
-app.use("/phamacyInventory",phamacyInventoryRouter);//call backend to frontend
-
-
-//access appoinment table file
-const appoinmentRouter = require("./routes/appoinment.js");
-//(http://localhost:8070/appoinment)we use this for routes.appoinment file
-app.use("/appoinment",appoinmentRouter);//call backend to frontend
-
-
-//access consultation table file
-const consultationRouter = require("./routes/consultation.js");
-//(http://localhost:8070/consultation)we use this for routes.appoinment file
-app.use("/consultation",consultationRouter);//call backend to frontend
-
-//access order table file
-const orderRouter = require("./routes/order.js");
-//(http://localhost:8070/order)we use this for routes.order file
-app.use("/order",orderRouter);//call backend to frontend
-
-app.listen(PORT, () => {
-    console.log('Server is up and running on port number: ${PORT}');
-})
+server();
